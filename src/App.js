2f21808e0d647fa89c7ea66e303d5812efb713b3
Bruno Buck <b.brigagao@gmail.com>
2018-07-31 16:27:54 -0300

    Alterações:
    - Button agora faz parte do componente Card;
    - Card carrega dados do planeta;
    - Chamada à API ok;
    
    #TODO:
    pegar um @planetaId passado pelo botão para usar na chamada à API;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button';
+// import Button from './components/Button';
 import Cards from './components/Cards';
 
 class App extends Component {
@@ -19,9 +19,9 @@ class App extends Component {
           <div className="row justify-content-center">
               <Cards />
           </div>
-          <div className="row justify-content-center">
+          {/* <div className="row justify-content-center">
               <Button />
-          </div>
+          </div> */}
         </div>
       </div>
     );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,97 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 class Cards extends Component {
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            planeta: []
+        };
+    }
+
+    componentDidMount() {
+        // let id = Math.floor(Math.random() * (61 - 1) + 1);
+        // console.log(id);
+        // fetch(`https://swapi.co/api/planets/${id}/`)
+        //     .then(res => res.json())
+        //     .then((result) => {
+        //         this.setState({
+        //             isLoaded: true,
+        //             planeta: result
+        //         });
+        //     }, (error) => {
+        //         this.setState({
+        //             isLoaded: true,
+        //             error
+        //         });
+        //     }
+        //     );
+        this.trocaPlaneta();
+    }
+
+    // componentWillReceiveProps(nextProps) {
 
     // }
 
+    // shouldComponentUpdate(nextProps, nextState) {
+
+    // }
+
+    // componentWillUpdate(nextProps, nextState) {
+
+    // }
+
+    // componentDidUpdate(prevProps, prevState) {
+
+    // }
+
+    // componentWillUnmount() {
+
+    // }
+
+    trocaPlaneta = () => {
+        let id = Math.floor(Math.random() * (61 - 1) + 1);
+        console.log(id);
+        fetch(`https://swapi.co/api/planets/${id}/`)
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    planeta: result
+                });
+            }, (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+            );
+    }
+
     render() {
-        return (
-            <div className="card mb-4 shadow-sm" /* style={{ width: `24rem` }}*/ >
-                <div className="card-header text-center">
-                    <h5 className="card-title">props.name</h5>
+        const { error, isLoaded, planeta } = this.state;
+        if (error) { return (<div className="alert alert-danger">Error: {error.message}</div>); }
+        else if (!isLoaded) { return <div>Carregando...</div>; }
+        else {
+            return (
+                <div>
+                    <div className="card mb-4 shadow-sm">
+                        <div className="card-header text-center">
+                            <h5 className="card-title">{planeta.name}</h5>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">{planeta.population}</p>
+                            <p className="card-text">{planeta.climate}</p>
+                            <p className="card-text">Apareceu em props.aparicoes Filmes</p>
+                        </div>
+                    </div>
+                    <Button />
                 </div>
-                <div className="card-body">
-                    <p className="card-text">props.population</p>
-                    <p className="card-text">props.climate</p>
-                    <p className="card-text">Apareceu em props.aparicoes Filmes</p>
-                </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
